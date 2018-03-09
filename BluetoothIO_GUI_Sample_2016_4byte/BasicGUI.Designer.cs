dcd20d24eacf83e4a8899dab0454b07fb58c2c06
artfuldodger51 <artfuldodger333@hotmail.com>
2018-03-09 11:11:50 +0800

    Initial commit

---
+++ BluetoothIO_GUI_Sample_2016_4byte/BasicGUI.Designer.cs
@@ -0,0 +1,384 @@
+﻿namespace BluetoothGUISample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.bluetooth = new System.IO.Ports.SerialPort(this.components);
+            this.getIOtimer = new System.Windows.Forms.Timer(this.components);
+            this.OutputBox1 = new System.Windows.Forms.NumericUpDown();
+            this.Send1 = new System.Windows.Forms.Button();
+            this.Send2 = new System.Windows.Forms.Button();
+            this.OutputBox2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.BitValue1 = new System.Windows.Forms.NumericUpDown();
+            this.Troubleshooting = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BitValue1)).BeginInit();
+            this.Troubleshooting.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bluetooth
+            // 
+            this.bluetooth.PortName = "COM4";
+            // 
+            // getIOtimer
+            // 
+            this.getIOtimer.Enabled = true;
+            this.getIOtimer.Interval = 10;
+            this.getIOtimer.Tick += new System.EventHandler(this.getIOtimer_Tick);
+            // 
+            // OutputBox1
+            // 
+            this.OutputBox1.Location = new System.Drawing.Point(83, 115);
+            this.OutputBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.OutputBox1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.OutputBox1.Name = "OutputBox1";
+            this.OutputBox1.Size = new System.Drawing.Size(250, 31);
+            this.OutputBox1.TabIndex = 3;
+            // 
+            // Send1
+            // 
+            this.Send1.Location = new System.Drawing.Point(373, 107);
+            this.Send1.Margin = new System.Windows.Forms.Padding(6);
+            this.Send1.Name = "Send1";
+            this.Send1.Size = new System.Drawing.Size(255, 44);
+            this.Send1.TabIndex = 4;
+            this.Send1.Text = "Change Position";
+            this.Send1.UseVisualStyleBackColor = true;
+            // 
+            // Send2
+            // 
+            this.Send2.Location = new System.Drawing.Point(373, 249);
+            this.Send2.Margin = new System.Windows.Forms.Padding(6);
+            this.Send2.Name = "Send2";
+            this.Send2.Size = new System.Drawing.Size(255, 44);
+            this.Send2.TabIndex = 4;
+            this.Send2.Text = "Change Velocity";
+            this.Send2.UseVisualStyleBackColor = true;
+            this.Send2.Click += new System.EventHandler(this.Send2_Click);
+            // 
+            // OutputBox2
+            // 
+            this.OutputBox2.Location = new System.Drawing.Point(83, 257);
+            this.OutputBox2.Margin = new System.Windows.Forms.Padding(6);
+            this.OutputBox2.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.OutputBox2.Name = "OutputBox2";
+            this.OutputBox2.Size = new System.Drawing.Size(250, 31);
+            this.OutputBox2.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(83, 391);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(250, 31);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(255, 44);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Change Acceleration";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(671, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 31);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(671, 257);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 31);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(671, 391);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 31);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(671, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Current Position";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(671, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Current Velocity";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(671, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Current Acceleration";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 25);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Next Position";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 227);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Next Velocity";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(88, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(181, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Next Acceleration";
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart1.Legends.Add(legend4);
+            this.chart1.Location = new System.Drawing.Point(1083, 48);
+            this.chart1.Name = "chart1";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart1.Series.Add(series4);
+            this.chart1.Size = new System.Drawing.Size(500, 400);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart2.Legends.Add(legend5);
+            this.chart2.Location = new System.Drawing.Point(1093, 555);
+            this.chart2.Name = "chart2";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart2.Series.Add(series5);
+            this.chart2.Size = new System.Drawing.Size(500, 400);
+            this.chart2.TabIndex = 17;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea6.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart3.Legends.Add(legend6);
+            this.chart3.Location = new System.Drawing.Point(517, 555);
+            this.chart3.Name = "chart3";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.chart3.Series.Add(series6);
+            this.chart3.Size = new System.Drawing.Size(500, 400);
+            this.chart3.TabIndex = 18;
+            this.chart3.Text = "chart3";
+            // 
+            // BitValue1
+            // 
+            this.BitValue1.Location = new System.Drawing.Point(14, 69);
+            this.BitValue1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.BitValue1.Name = "BitValue1";
+            this.BitValue1.Size = new System.Drawing.Size(151, 31);
+            this.BitValue1.TabIndex = 19;
+            // 
+            // Troubleshooting
+            // 
+            this.Troubleshooting.Controls.Add(this.button2);
+            this.Troubleshooting.Controls.Add(this.label7);
+            this.Troubleshooting.Controls.Add(this.BitValue1);
+            this.Troubleshooting.Location = new System.Drawing.Point(26, 508);
+            this.Troubleshooting.Name = "Troubleshooting";
+            this.Troubleshooting.Size = new System.Drawing.Size(346, 447);
+            this.Troubleshooting.TabIndex = 21;
+            this.Troubleshooting.TabStop = false;
+            this.Troubleshooting.Text = "Troubleshooting";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(186, 64);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 42);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(14, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(151, 25);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Bit Value Input";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1641, 1010);
+            this.Controls.Add(this.Troubleshooting);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.Send2);
+            this.Controls.Add(this.Send1);
+            this.Controls.Add(this.OutputBox2);
+            this.Controls.Add(this.OutputBox1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OutputBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BitValue1)).EndInit();
+            this.Troubleshooting.ResumeLayout(false);
+            this.Troubleshooting.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer getIOtimer;
+        private System.Windows.Forms.NumericUpDown OutputBox1;
+        private System.IO.Ports.SerialPort bluetooth;
+        private System.Windows.Forms.Button Send1;
+        private System.Windows.Forms.Button Send2;
+        private System.Windows.Forms.NumericUpDown OutputBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.NumericUpDown BitValue1;
+        private System.Windows.Forms.GroupBox Troubleshooting;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
